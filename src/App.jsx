e34287d206da8471cfd171b2f1f7651601871a2a
Ashton Sloan <sloan1294@hotmail.com>
2023-07-28 10:15:31 -0500

    Second Commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,19 @@
 import React from "react";
-
+import Navbar from "./components/Navbar"
+import Hero from "./components/Hero"
+import Projects from "./components/Projects"
+import About from "./components/About";
+import Contact from "./components/Contact";
 
 
 const App = () => {
   return (
     <main>
-      <div className="mt-[20%] text-center text-[#49c5b6]">
-        <h1 className=" w-full pr-22 text-7xl font-bold text-[#49c5b6]  border-b-4 border-[#49c5b6]">
-          
-            Keep Moving Forward.
-       
-        </h1>
-       </div>
+      <Navbar />
+      <Hero />
+      <Projects />
+      <About />
+      <Contact />
     </main>
   );
 }
