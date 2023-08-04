3f6193e5d9be3bdb7dbe0daf1c43bfc7b1f8515c
Ashton Sloan <sloan1294@hotmail.com>
2023-08-03 21:22:48 -0500

    Second Commit

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -20,11 +20,11 @@ const Projects = () => {
               <p>Budgeting done better</p>
             </div>
             <p className="text-white text-center text-base mb-1">
-              (No Template) TracMate is a budgeting app that allows you to create an account
-              protected by firebase. From there you can go stright to entering
-              your budget on the next page. After filling out your budget you
-              can then hit a button to create a pie chart that uses the data you
-              entered.
+              (No Template) TracMate is a budgeting app that allows you to
+              create an account protected by firebase. From there you can go
+              stright to entering your budget on the next page. After filling
+              out your budget you can then hit a button to create a pie chart
+              that uses the data you entered.
             </p>
             <p className="text-white text-center text-base mb-1 pt-2">
               <b className="text-[#49c5b6]">Challanges: </b>Creating the chart
@@ -34,10 +34,16 @@ const Projects = () => {
             </p>
           </div>
           <div className="flex justify-center">
-            <a href="https://joyful-khapse-96fe02.netlify.app/" className="bunk text-white bg-[#49c5b6] hover:bg-green-400 font-bold py-2 px-8 m-5 rounded-lg ">
+            <a
+              href="https://joyful-khapse-96fe02.netlify.app/"
+              className="bunk text-white bg-[#49c5b6] hover:bg-green-400 font-bold py-2 px-8 m-5 rounded-lg "
+            >
               Live Site
             </a>
-            <a href="https://github.com/sloan1294/TracMate" className="bunk text-white bg-[#49c5b6] hover:bg-green-400 font-bold py-2 px-9 m-5 rounded-lg ">
+            <a
+              href="https://github.com/sloan1294/TracMate"
+              className="bunk text-white bg-[#49c5b6] hover:bg-green-400 font-bold py-2 px-9 m-5 rounded-lg "
+            >
               Github
             </a>
           </div>
@@ -62,27 +68,35 @@ const Projects = () => {
           />
           <div class="px-2 py-6">
             <div class="font-bold text-center text-xl mb-5 text-[#49c5b6]">
-              <p >Z-Adventures.</p>
-              <p className="max-[550px]:px-10">An innovative way to experience the outdoors.</p>
+              <p>Z-Adventures.</p>
+              <p className="max-[550px]:px-10">
+                An innovative way to experience the outdoors.
+              </p>
             </div>
             <p className="max-[550px]:px-10 text-white text-center text-base m-6">
-              Z-Adventures is an adventure oriented equipment store. Having all items
-              from heavy duty boots to Winter jackets we provide for any
+              Z-Adventures is an adventure oriented equipment store. Having all
+              items from heavy duty boots to Winter jackets we provide for any
               environment imaginable.
             </p>
             <p className="text-white text-center text-base mb-1 pt-2 max-[550px]:px-10">
-              <b className="text-[#49c5b6]">Challanges: </b> Z-Adventures is a project
-              I built to understand HTML/PUG landing pages better, while also having a
-              reason to build a working cart. This turned out to be quite the
-              challange for me. Hours later I finally walked
-              away with a soultion I was satisfied with!
+              <b className="text-[#49c5b6]">Challanges: </b> Z-Adventures is a
+              project I built to understand HTML/PUG landing pages better, while
+              also having a reason to build a working cart. This turned out to
+              be quite the challange for me. Hours later I finally walked away
+              with a soultion I was satisfied with!
             </p>
           </div>
           <div className="flex justify-center">
-            <a href="https://main--radiant-crumble-ae8f74.netlify.app/" className="bunk text-white bg-[#49c5b6] hover:bg-green-400 font-bold py-2 px-8 m-5 rounded-lg ">
+            <a
+              href="https://main--radiant-crumble-ae8f74.netlify.app/"
+              className="bunk text-white bg-[#49c5b6] hover:bg-green-400 font-bold py-2 px-8 m-5 rounded-lg "
+            >
               Live Site
             </a>
-            <a href="https://github.com/sloan1294/Z-Adventures" className="bunk text-white bg-[#49c5b6] hover:bg-green-400 font-bold py-2 px-9 m-5 rounded-lg ">
+            <a
+              href="https://github.com/sloan1294/Z-Adventures"
+              className="bunk text-white bg-[#49c5b6] hover:bg-green-400 font-bold py-2 px-9 m-5 rounded-lg "
+            >
               Github
             </a>
           </div>
@@ -111,23 +125,30 @@ const Projects = () => {
               <p>Trying the basics.</p>
             </div>
             <p className="text-white text-center text-base m-6">
-              (No Template) This is a weather app that will tell you the current temputure in
-              the city you search. Using the Gps image on the left hand side of
-              the input area will allow you to get your current location.
+              (No Template) This is a weather app that will tell you the current
+              temputure in the city you search. Using the Gps image on the left
+              hand side of the input area will allow you to get your current
+              location.
             </p>
             <p className="text-white text-center text-base mb-1 pt-2">
-              <b className="text-[#49c5b6]">Challanges: </b> Overall this
+              <b className="text-[#49c5b6]">Challanges: </b> Overall, this
               project was not too difficult. Connecting to the open weather Api
-              was stright forward, and retriving that data was explained well in
-              the docs. Calling the second Api and returning the correct
-              data(kept returning wrong location) was quite a bit harder!
+              was straight forward, and retrieving that data was explained well
+              in the docs. Calling the second Api and returning the correct data
+              (kept returning wrong location) was quite a bit harder!
             </p>
           </div>
           <div className="flex justify-center">
-            <a href="https://silver-raindrop-2097fc.netlify.app/" className="bunk text-white bg-[#49c5b6] hover:bg-green-400 font-bold py-2 px-8 m-5 rounded-lg ">
+            <a
+              href="https://silver-raindrop-2097fc.netlify.app/"
+              className="bunk text-white bg-[#49c5b6] hover:bg-green-400 font-bold py-2 px-8 m-5 rounded-lg "
+            >
               Live Site
             </a>
-            <a href="https://github.com/sloan1294/Weather-App" className="bunk text-white bg-[#49c5b6] hover:bg-green-400 font-bold py-2 px-9 m-5 rounded-lg ">
+            <a
+              href="https://github.com/sloan1294/Weather-App"
+              className="bunk text-white bg-[#49c5b6] hover:bg-green-400 font-bold py-2 px-9 m-5 rounded-lg "
+            >
               Github
             </a>
           </div>
